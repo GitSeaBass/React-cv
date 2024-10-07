6dfed1520429ba1df4ec7d371172bcd916174f2d
Sebastian Garcia <sag44402@uga.edu>
2024-10-07 17:36:07 -0400

    testing

--- react-cv/src/App.js
+++ react-cv/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and extra.
+          Edit <code>src/App.js</code> and save to reload and extra!.
         </p>
         <a
           className="App-link"
