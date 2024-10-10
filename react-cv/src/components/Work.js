2e576489f82206738c3cf3a9f0b46b5bd26f9d87
Sebastian Garcia <sag44402@uga.edu>
2024-10-10 17:38:41 -0400

    changed layout of work expeirence page

--- react-cv/src/components/Work.js
+++ react-cv/src/components/Work.js
@@ -7,37 +7,38 @@ function Work({workRef}) {
 
             <div className='jobs-container'>
                 <div className='job'>
-                    <img className='job-img' src='https://a.storyblok.com/f/201186/1200x680/6cb4c20358/opengraph-default-image.png' alt='Altitude'/>
-
-                    <div className='job-description'>
-                        <div className='job-header'>
-                            <h3>Court Monitor</h3>
-                            <h4>June 2021 - July 2021</h4>
+                    <div className='job-header'>
+                        <div>
+                            <h3 className='header-remove-margin'>Altitude</h3>
+                            <h5 className='header-remove-margin'>Court Monitor</h5>
+                            <h5 className='header-remove-margin'>June 2021 - July 2021</h5>
                         </div>
-                        
-                        <ul>
-                            <li>Ensured customers followed the rules while jumping on trampolines to ensure their safety</li>
-                            <li>Worked with customers to resolve issues</li>
-                            <li>Communicated with team members to relay questions and emergencies as well as to coordinate with each other</li>
-                        </ul>
+                        <img className='job-img' src='https://a.storyblok.com/f/201186/1200x680/6cb4c20358/opengraph-default-image.png' alt='Altitude'/>
                     </div>
+                    
+                    <ul>
+                        <li>Ensured customers followed the rules while jumping on trampolines to ensure their safety</li>
+                        <li>Worked with customers to resolve issues</li>
+                        <li>Communicated with team members to relay questions and emergencies as well as to coordinate with each other</li>
+                    </ul>
+                    
                 </div>
 
                 <div className='job'>
-                    <img className='job-img' src='https://logos-world.net/wp-content/uploads/2021/11/YMCA-Logo.png' alt='YMCA'/>
-
-                    <div className='job-description'>
                     <div className='job-header'>
-                            <h3>Assistant Camp Counselor</h3>
-                            <h4>June 2019 - July 2019</h4>
+                        <div>
+                            <h3 className='header-remove-margin'> YMCA</h3>
+                            <h5 className='header-remove-margin'>Assistant Camp Counselor</h5>
+                            <h5 className='header-remove-margin'>June 2019 - July 2019</h5>
                         </div>
-                        
-                        <ul>
-                            <li>Supervised a group of 20 children, ages 5 - 14</li>
-                            <li>Provided the children with the ability to participate in camp activities while ensuring their safety and resolving conflicts</li>
-                            <li>Organized and supervised children during activities including rock climbing, swimming, and archery</li>
-                        </ul>
+                        <img className='job-img' src='https://logos-world.net/wp-content/uploads/2021/11/YMCA-Logo.png' alt='YMCA'/>
                     </div>
+                        
+                    <ul>
+                        <li>Supervised a group of 20 children, ages 5 - 14</li>
+                        <li>Provided the children with the ability to participate in camp activities while ensuring their safety and resolving conflicts</li>
+                        <li>Organized and supervised children during activities including rock climbing, swimming, and archery</li>
+                    </ul>
                 </div>
             </div>
         </div>
