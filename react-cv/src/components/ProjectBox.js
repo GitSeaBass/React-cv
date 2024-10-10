639f01f7d91369f0381160e2763fa2e2926f0be4
Sebastian Garcia <sag44402@uga.edu>
2024-10-10 19:00:17 -0400

    filled in projects with myProjects data

--- react-cv/src/components/ProjectBox.js
+++ react-cv/src/components/ProjectBox.js
@@ -1,21 +1,21 @@
 import './ProjectBox.css'
 
-function ProjectBox() {
+function ProjectBox({ title, des, link, codes}) {
     return (
         <div className='project'>
             <div className='project-header'>
-                <h4 className='project-title'>Cinema E-Booking System</h4>
-                <div className='project-link'>Link</div>
+                <h4 className='project-title'>{title}</h4>
+                <div className='project-link'>{link}</div>
             </div>
             <ul className='project-description'>
-                <li>I did things</li>
-                <li>some other things</li>
-                <li>You wont believe it</li>
+                <li>{des}</li>
             </ul>
             <div className='project-languages'>
-                <div>Lang</div>
-                <div>Lang</div>
-                <div>Lang</div>
+                {codes.map((code) => 
+                    <div>
+                        <img className='project-code-img' src={code} alt="Lang"/>
+                    </div>
+                )}
             </div>
         </div>
     )
--- react-cv/src/components/Projects.js
+++ react-cv/src/components/Projects.js
@@ -12,11 +12,8 @@ function Projects({projectsRef}) {
                 'https://pbs.twimg.com/profile_images/1235868806079057921/fTL08u_H_400x400.png',
                 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png',
                 'https://www.svgrepo.com/show/303251/mysql-logo.svg'
-
             ],
-            links: [
-
-            ],
+            link: "",
             des: "Worked within a 5 person group to create a full-stack cinema e-booking system. This application uses React for the frontend while using SpringBoot for the backend along with a MySQL database that stores movies and users. I primarily worked on the frontend, but assisted elsewhere when needed. We worked through deliverables by delegating work between ourselves and communicating the progress of our work."
         },
         {
@@ -28,9 +25,7 @@ function Projects({projectsRef}) {
                 'https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Logo.png',
                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBwzWqFVu66ck-2u_nDBgLTZbR3cNjpUCbWg&s',
             ],
-            links: [
-                
-            ],
+            link: "",
             des: "Worked within a group of 3 to create an image gallery where users can upload photos and view other users photos. React was used for the frontend while Node and MongoDB were used for the backend and database."
         },
         {
@@ -39,9 +34,7 @@ function Projects({projectsRef}) {
                 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png',
                 'https://upload.wikimedia.org/wikipedia/commons/9/92/LaTeX_logo.svg'
             ],
-            links: [
-                
-            ],
+            link: "",
             des: "Used multiple genetic algorithms to find the best Pokemon team to face a series of trainers along with writing a research paper outlining the process, design elements, and results of the genetic algorithm."
         }
 
@@ -54,15 +47,15 @@ function Projects({projectsRef}) {
 
             <div className='projects-container'>
                 <div className='project-row'>
-                    <ProjectBox />
+                    <ProjectBox title={myProjects[0].title} des={myProjects[0].des} link={myProjects[0].link} codes={myProjects[0].codes} />
                     <div className='fill-div'></div>
                 </div>
                 <div className='project-row'>
                     <div className='fill-div'></div>
-                    <ProjectBox />
+                    <ProjectBox title={myProjects[1].title} des={myProjects[1].des} link={myProjects[1].link} codes={myProjects[1].codes}/>
                 </div>
                 <div className='project-row'>
-                    <ProjectBox />
+                    <ProjectBox title={myProjects[2].title} des={myProjects[2].des} link={myProjects[2].link} codes={myProjects[2].codes}/>
                     <div className='fill-div'></div>
                 </div>
             </div>
